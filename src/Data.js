587d6134740c6a7b680642932b983e70599b6bfb
Marvenomous <167874295+Marvenomous@users.noreply.github.com>
2024-05-24 10:33:47 +0100

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "An all rounder casual shirt for everyday wear.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office Shirt",
+    price: 100,
+    desc: "Whether at work or at a formal event, this shirt will be handy.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "T Shirt",
+    price: 25,
+    desc: "Simple yet stylish.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens Suit",
+    price: 300,
+    desc: "Rich fabric with elegant style and touch",
+    img: "images/img-4.jpg",
+  },
+];
